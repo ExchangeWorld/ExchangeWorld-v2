70a63d3eaa28481a9886066c27456384fbf06111
Xhriman <seal789ie@gmail.com>
2015-11-12 10:49:53 +0800

    e2e test

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -3,14 +3,14 @@ const webpack = require('webpack');
 
 module.exports = {
 	devtool: 'source-map',
-	entry: [
-		'webpack-hot-middleware/client',
-		'./src/index.jsx'
-	],
+	entry: {
+		app: './src/app/index.jsx',
+		admin: './src/admin/index.jsx',
+	},
 	output: {
 		path: path.join(__dirname, 'build'),
-		filename: 'main.js',
-		publicPath: '/static/'
+		filename: '[name].bundle.js',
+		publicPath: 'static/'
 	},
 	plugins: [
 		new webpack.optimize.OccurenceOrderPlugin(),
