c5e9c496ae37703bd2b8bbf7874e76b676b99018
Xhriman <seal789ie@gmail.com>
2016-02-29 15:16:47 +0800

    update webpack

--- webpack.config.build.js
+++ webpack.config.build.js
@@ -1,27 +1,35 @@
-const path = require('path');
-const webpack = require('webpack');
-const nodeModules = path.join(__dirname, 'node_modules');
-const pathToReactDOM = path.resolve(nodeModules, 'react-dom/dist/react-dom.min.js');
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var nodeModules = path.join(__dirname, 'node_modules');
+var pathToReactDOM = path.resolve(nodeModules, 'react-dom/dist/react-dom.min.js');
 
-const deps = [
-  'react/dist/react.min.js',
+var deps = [
+	'react/dist/react.min.js',
+	'i18next/bin/index.js',
+	'i18next-xhr-backend/bin/index.js',
+	'i18next-localstorage-cache/bin/index.js',
 ];
 
-const config = {
-	devtool: 'source-map',
-	entry: {
-		app: './src/app/index.jsx',
-		admin: './src/admin/index.jsx',
+var config = {
+	devtool: 'eval',
+	entry:  {
+		app: [
+			'./src/app/index.jsx'
+		],
+		admin: [
+			'./src/admin/index.jsx'
+		],
 	},
 	resolve: {
 		alias: {
 			'react-dom': pathToReactDOM,
-		}
+		},
+		extensions: ["", ".jsx", ".js", ".scss"],
 	},
 	output: {
-		path: path.join(__dirname, 'build'),
-		filename: '[name].bundle.js',
-		publicPath: '/static/'
+		path: path.join(__dirname, 'dist/public/'),
+		filename: 'js/[name].js',
 	},
 	plugins: [
 		new webpack.optimize.OccurenceOrderPlugin(),
@@ -30,6 +38,7 @@ const config = {
 				'NODE_ENV': JSON.stringify('production')
 			}
 		}),
+		new webpack.optimize.DedupePlugin(),
 		new webpack.optimize.UglifyJsPlugin({
 			compressor: {
 				warnings: false
@@ -43,23 +52,47 @@ const config = {
 				loaders: ['babel'],
 				include: path.join(__dirname, 'src'),
 				exclude: path.join(__dirname, 'node_modules'),
-			}, 
+			},
+			{
+				test: /\.css$/,
+				loaders: [
+					'style-loader',
+					'css-loader',
+					'postcss-loader'
+				],
+				include: path.join(__dirname, 'style'),
+			},
 			{
 				test: /\.scss$/,
-				loader: "style-loader!css-loader!sass-loader!autoprefixer-loader?browsers=last 2 versions",
+				loaders: [
+					'style-loader',
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
 				include: path.join(__dirname, 'style'),
-			}, 
-			{ 
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-				loader: "url-loader?limit=10000&mimetype=application/font-woff&name=[name].[ext]" 
 			},
-			{ 
-				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-				loader: "file-loader&name=[name].[ext]" 
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]"
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "file-loader?name=fonts/[name].[ext]"
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file?hash=sha512&digest=hex&name=imgs/[hash].[ext]',
+					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+				]
 			}
 		],
 		noParse: [],
-	}
+	},
+	postcss: function () {
+		return [autoprefixer];
+	},
 };
 
 deps.forEach(function (dep) {
@@ -67,6 +100,5 @@ deps.forEach(function (dep) {
 	config.resolve.alias[dep.split(path.sep)[0]] = depPath;
 	config.module.noParse.push(depPath);
 });
-
 
 module.exports = config;
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,33 +1,40 @@
-const path = require('path');
-const webpack = require('webpack');
-const nodeModules = path.join(__dirname, 'node_modules');
-const pathToReactDOM = path.resolve(nodeModules, 'react-dom/dist/react-dom.min.js');
+var path = require('path');
+var webpack = require('webpack');
+var autoprefixer = require('autoprefixer');
+var nodeModules = path.join(__dirname, 'node_modules');
+var pathToReactDOM = path.resolve(nodeModules, 'react-dom/dist/react-dom.min.js');
 
-const deps = [
-  'react/dist/react.min.js',
+var deps = [
+	// 'react/dist/react.min.js',
+	'i18next/bin/index.js',
+	'i18next-xhr-backend/bin/index.js',
+	'i18next-localstorage-cache/bin/index.js',
 ];
 
-const config = {
-	devtool: 'cheap-module-eval-source-map',
-	entry: {
+var config = {
+	devtool: 'eval',
+	entry:  {
 		app: [
-			'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
+			'eventsource-polyfill',
+			'webpack-hot-middleware/client?path=http://localhost:8080/__webpack_hmr',
 			'./src/app/index.jsx'
 		],
 		admin: [
-			'webpack-hot-middleware/client?path=http://localhost:3000/__webpack_hmr',
+			'eventsource-polyfill',
+			'webpack-hot-middleware/client?path=http://localhost:8080/__webpack_hmr',
 			'./src/admin/index.jsx'
-		],
+		]
 	},
 	resolve: {
 		alias: {
 			'react-dom': pathToReactDOM,
-		}
+		},
+		extensions: ["", ".jsx", ".js", ".scss"],
 	},
 	output: {
-		path: path.join(__dirname, 'build'),
-		filename: '[name].bundle.js',
-		publicPath: '/static/'
+		path: path.join(__dirname, 'dist/public'),
+		filename: 'js/[name].js',
+		publicPath: '/public'
 	},
 	plugins: [
 		new webpack.HotModuleReplacementPlugin(),
@@ -40,30 +47,53 @@ const config = {
 				loaders: ['babel'],
 				include: path.join(__dirname, 'src'),
 				exclude: path.join(__dirname, 'node_modules'),
-			}, 
+			},
+			{
+				test: /\.css$/,
+				loaders: [
+					'style-loader',
+					'css-loader?sourceMap',
+					'postcss-loader',
+				],
+				include: path.join(__dirname, 'style'),
+			},
 			{
 				test: /\.scss$/,
-				loader: "style-loader!css-loader!sass-loader!autoprefixer-loader?browsers=last 2 versions",
+				loaders: [
+					'style-loader',
+					'css-loader',
+					'postcss-loader',
+					'sass-loader',
+				],
 				include: path.join(__dirname, 'style'),
-			}, 
-			{ 
-				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-				loader: "url-loader?limit=10000&mimetype=application/font-woff" 
 			},
-			{ 
-				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, 
-				loader: "file-loader" 
+			{
+				test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[name].[ext]"
+			},
+			{
+				test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+				loader: "file-loader?name=fonts/[name].[ext]"
+			},
+			{
+				test: /\.(jpe?g|png|gif|svg)$/i,
+				loaders: [
+					'file?hash=sha512&digest=hex&name=imgs/[hash].[ext]',
+					'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+				]
 			}
 		],
 		noParse: [],
-	}
+	},
+	postcss: function () {
+		return [autoprefixer];
+	},
 };
 
 deps.forEach(function (dep) {
-  var depPath = path.resolve(nodeModules, dep);
-  config.resolve.alias[dep.split(path.sep)[0]] = depPath;
-  config.module.noParse.push(depPath);
+	var depPath = path.resolve(nodeModules, dep);
+	config.resolve.alias[dep.split(path.sep)[0]] = depPath;
+	config.module.noParse.push(depPath);
 });
-
 
 module.exports = config;
